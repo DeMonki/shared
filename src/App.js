da472e0366c24a517627c1a775ab997fd6ce0081
DeMonki <dave@fretlow.com>
2022-02-05 22:08:20 -0600

    add home lose create react remnants
    FreddyTheFrog.jpg
    
    feat: added meals component (JIRA-TAG)
    
    need to force push because this branch is deleted
    
    feat: video added needs cleaning up
    
    changes a few words
    
    adds words to main branch
    
    not an app
    
    doubles added words
    
    git whacheck
    
    new stuff
    
    more on main
    
    create a problem
    
    adding to problem
    
    adds words to main branch
    
    not an app
    
    doubles added words
    
    new stuff
    
    more on main
    
    one change
    
    two change
    
    three change
    
    adds name
    
    adds another name
    
    removes content

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import './App.css';
 import Elements from './Elements';
 import Home from './Home';
 import Meals from './Meals';
+import Name from './Name';
 
 function App() {
   return (
     <div className="App">
+      <Name />
+      <Name />
       <Elements />
        on main.
       <Home />
---
+++ src/Name.js
@@ -0,0 +1,9 @@
+const Name = () => {
+
+    return (
+        <div>
+        <h1>My name is: </h1>
+        </div>
+    );
+    }
+    export default Name;
